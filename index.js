2f3076248894ad0049d76933d15f0d2f5ad3ea56
BezAlex <100188945+BezAlex@users.noreply.github.com>
2022-03-08 12:16:05 +0300

    deploy 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!, test deploy 1')
+  res.send('Hello World!, test deploy 2')
 })
 
 app.listen(port, () => {
